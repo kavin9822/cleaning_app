6dd382a61262ec7b407708be2f8da28e5f0cb909
sasmithakarthi <sasmithasasmi5301@gmail.com>
2023-07-20 12:11:33 +0530

    for update

---
+++ api/v1/lib/config/database.config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright Copyright (c) 2014-2016 www.whyceeyes.com 
+ * 
+ */
+
+return [
+    'database_type' => 'mysql',
+    'database_name' => 'hk',
+    'server' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'port' => 3306,
+    'prefix' => 'ycias_',
+    'option' => [
+        PDO::ATTR_CASE => PDO::CASE_NATURAL,
+    ]
+];
---
+++ lib/ajax/set_inc_path.php
@@ -0,0 +1,9 @@
+<?php
+#development
+set_include_path(".;C:/xampp/htdocs/cleaning_app/lib");
+
+#production
+//set_include_path(".:/var/app/current/lib");
+
+//Set default local timezone(SGT) settings.
+date_default_timezone_set('Asia/Singapore');
